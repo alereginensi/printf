f0d560cb9f750aa25afacde344e0279989fb6b3e
alereginensi <reginensia@gmail.com>
2022-03-11 11:25:20 -0800

    task 0

---
+++ 0-print_format.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _printf - produces output according to a format.
+ * @format: list of arguments tpes passed to the function.
+ *
+ */
+int _printf(const char *format, ...)
+{
+	va_list charac
+	
+	int _printf("%d", format)
+}
+
