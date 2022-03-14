1b953ba503cd415ec6be5b2a9d4b0ac00dafd40b
alereginensi <reginensia@gmail.com>
2022-03-14 16:12:28 -0700

    betty fixes for _printf

--- _printf.c
+++ _printf.c
@@ -1,36 +1,7 @@
 #include "main.h"
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-/**
- * get_identifier - gets the correct identifier to print
- * @formatt: type of format
- * Return: pointer to function
- */
-int (*get_identifier(char formatt))(va_list)
-{
-	print_f letter[] = {
-		{'c', c_printf},
-		{'s', s_printf},
-		{'i', number_printf},
-		{'d', number_printf},
-		{'\0', NULL},
-	};
-	int i = 0;
-
-	for (i = 0; letter[i].string; i++)
-	{
-		if (formatt == letter[i].string)
-			return (letter[i].func);
-	}
-	return (NULL);
-}
-
 /**
  * _printf - produces output according to a format.
  * @format: list of arguments tpes passed to the function.
- *
  * Return: Same as printf standard functions
  */
 int _printf(const char *format, ...)
@@ -39,23 +10,21 @@ int _printf(const char *format, ...)
 	int i = 0, counter = 0;
 	int (*func)(va_list);
 
-	if (!format || (format[0] == '%' && format [1] == '\0'))
+	if (!format || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 	va_start(arg, format);
 	for (; format && format[i]; i++)
 	{
 		if (format[i] == '%')
 		{
-			if (format [i + 1] == '%')
+			if (format[i + 1] == '%')
 			{
-				_putchar(format[i]);
-				counter++;
+				_putchar(format[i]), counter++;
 				continue;
 			}
 			else if (format[i + 1] == '\0')
 			{
-				_putchar(format[i]);
-				counter++;
+				_putchar(format[i]), counter++;
 				return (counter);
 			}
 			else
@@ -63,22 +32,14 @@ int _printf(const char *format, ...)
 				func = get_identifier(format[i + 1]);
 				if (func != NULL)
 				{
-					counter += func(arg);
-					i++;
+					counter += func(arg), i++;
 				}
 				else
 				{
-					_putchar(format[i]);
-					counter++;
-				}
-			}
-		}
+					_putchar(format[i]), counter++;		}	}	}
 		else
 		{
-			_putchar(format[i]);
-			counter++;
-		}	
-	}
+			_putchar(format[i]), counter++;		}	}
 	va_end(arg);
 	return (counter);
 }
