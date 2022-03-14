7f3cb9fd26392512da7c00e1167c1380dcc2b9fb
alereginensi <reginensia@gmail.com>
2022-03-14 07:01:30 -0700

    printf fix

---
+++ get_identifier.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+int (*get_identifier(char *s))(int, int);
+{
+	print_f letter[] = {
+		{'c', c_printf},
+		{'s', s_printf},
+		{'%', percent_printf},
+		{'i', number_printf},
+		{'d', number_printf},
+		{'u', unsigned_printf},
+		{'r', reverse_printf},
+		{'\0', NULL},
+};
