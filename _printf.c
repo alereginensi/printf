0c2fe5f774167eac5bf2619dcacb526f1d120948
alereginensi <reginensia@gmail.com>
2022-03-14 16:19:51 -0700

    betty _printf

--- _printf.c
+++ _printf.c
@@ -1,4 +1,32 @@
 #include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * get_identifier - gets the correct identifier to print
+ * @formatt: type of format
+ * Return: pointer to function
+ */
+int (*get_identifier(char formatt))(va_list)
+{
+	print_f letter[] = {
+		{'c', c_printf},
+		{'s', s_printf},
+		{'i', number_printf},
+		{'d', number_printf},
+		{'\0', NULL},
+	};
+	int i = 0;
+
+	for (i = 0; letter[i].string; i++)
+	{
+		if (formatt == letter[i].string)
+			return (letter[i].func);
+	}
+	return (NULL);
+}
+
 /**
  * _printf - produces output according to a format.
  * @format: list of arguments tpes passed to the function.
